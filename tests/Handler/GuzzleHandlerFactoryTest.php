3dfc30a16fee12a429ad1752befebb980bf0a2a0
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2019-05-19 17:39:40 -0300

    Add tests for GuzzleHandlerFactory

---
+++ tests/Handler/GuzzleHandlerFactoryTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This file is part of coisa/http.
+ *
+ * (c) Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+declare(strict_types=1);
+
+namespace CoiSA\Http\Test;
+
+use CoiSA\Http\Client\GuzzleHandlerFactory;
+use CoiSA\Http\Handler\GuzzleHandler;
+use GuzzleHttp\ClientInterface;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Class GuzzleHandlerFactoryTest
+ *
+ * @package CoiSA\Http\Test
+ */
+final class GuzzleHandlerFactoryTest extends TestCase
+{
+    /** @var ContainerInterface|ObjectProphecy */
+    private $container;
+
+    /** @var ObjectProphecy|ClientInterface */
+    private $client;
+
+    /** @var GuzzleHandlerFactory */
+    private $factory;
+
+    public function setUp(): void
+    {
+        $this->container = $this->prophesize(ContainerInterface::class);
+        $this->client    = $this->prophesize(ClientInterface::class);
+
+        $this->factory = new GuzzleHandlerFactory();
+
+        $this->container->get(ClientInterface::class)->shouldBeCalledOnce()->will([$this->client, 'reveal']);
+    }
+
+    public function testFactoryWithGuzzleClientWillReturnGuzzleHandler(): void
+    {
+        $client = ($this->factory)($this->container->reveal());
+
+        $this->assertInstanceOf(GuzzleHandler::class, $client);
+    }
+
+    public function testFactoryRaiseTypeErrorWhenContainerGetReturnUnexpectedObject(): void
+    {
+        $this->container->get(ClientInterface::class)->willReturn(\uniqid(__METHOD__, true));
+
+        $this->expectException(\TypeError::class);
+
+        ($this->factory)($this->container->reveal());
+    }
+}
