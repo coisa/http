4f849e08d46c896e292a77aeed87c9b7cfbdc6ae
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2019-05-19 18:26:49 -0300

    Fix ErrorHandlerMiddlewareTest

--- tests/Middleware/ErrorHandlerMiddlewareTest.php
+++ tests/Middleware/ErrorHandlerMiddlewareTest.php
@@ -15,7 +15,10 @@ namespace CoiSA\Http\Test\Middleware;
 
 use CoiSA\Http\Middleware\ErrorHandlerMiddleware;
 use CoiSA\Http\Test\Handler\AbstractMiddlewareTest;
+use Phly\EventDispatcher\EventDispatcher;
 use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
+use Psr\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Class ErrorHandlerMiddlewareTest
@@ -24,27 +27,43 @@ use Prophecy\Argument;
  */
 final class ErrorHandlerMiddlewareTest extends AbstractMiddlewareTest
 {
+    /** @var EventDispatcher|ObjectProphecy */
+    private $eventDispatcher;
+
     public function setUp(): void
     {
         parent::setUp();
 
-        $this->middleware = new ErrorHandlerMiddleware($this->handler->reveal());
+        $this->eventDispatcher = $this->prophesize(EventDispatcherInterface::class);
 
         $this->serverRequest->withAttribute(Argument::type('string'), Argument::type(\Throwable::class))->will([$this->serverRequest, 'reveal']);
+
+        $this->middleware = new ErrorHandlerMiddleware(
+            $this->handler->reveal(),
+            $this->eventDispatcher->reveal()
+        );
     }
 
-    public function testSuccessExecutionReturnHandlerResponse(): void
+    public function testProcessWillReturnHandlerResponse(): void
     {
         $response = $this->middleware->process($this->serverRequest->reveal(), $this->nextHandler->reveal());
+
         $this->assertSame($this->nextResponse->reveal(), $response);
         $this->assertNotSame($this->response->reveal(), $response);
     }
 
-    public function testOnExceptionReturnErrorHandlerResponse(): void
+    public function testProcessWillReturnErrorHandlerResponseWhenHandlerRaisesException(): void
     {
-        $this->nextHandler->handle($this->serverRequest->reveal())->willThrow(\Exception::class);
+        $throwable = new \Exception(
+            \uniqid('test', true),
+            \random_int(1, 500)
+        );
+
+        $this->eventDispatcher->dispatch($throwable)->shouldBeCalledOnce();
+        $this->nextHandler->handle($this->serverRequest->reveal())->willThrow($throwable);
 
         $response = $this->middleware->process($this->serverRequest->reveal(), $this->nextHandler->reveal());
+
         $this->assertSame($this->response->reveal(), $response);
         $this->assertNotSame($this->nextResponse->reveal(), $response);
     }
